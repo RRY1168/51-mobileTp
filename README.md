a6f6a76c5bd512b5b4b5094ee87e389da071e5a5
沈丹 <daniel.shen@chendandeMacBook-Pro.local>
2019-03-05 17:00:30 +0800

    添加基本插件 实现基本功能，可自定义模板（/template目录内） $ npm run build 可实现自动创建dist目录，同时创建指定文件 $ npm start 不创建文件但可直接运行页面效果 [[[ V0.1.0 完成 ]]]

---
+++ README.md
@@ -0,0 +1,34 @@
+#说明
+---------
+
+##目的
+本功能主要依托于git+webpack+NodeJS来初始化项目
+
+##配置要求
+1. NodeJS
+
+##命令
+```
+$ npm run build
+$ npm start
+```
+
+##V0.10
+>自动创建H5项目内容，包含：
+
+1. 生成适用于H5开发的静态页面
+2. 根据配置添加CSS、JavaScript
+
+>包含命令
+
+```
+$ npm install --save-dev webpack
+$ npm install --save-dev webpack-cli
+$ npm install --save-dev lodash
+$ npm install --save-dev style-loader
+$ npm install --save-dev css-loader
+$ npm install --save-dev file-loader
+$ npm install --save-dev html-webpack-plugin
+$ npm install --save-dev clean-webpack-plugin
+$ npm install --save-dev webpack-dev-server
+```
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+    devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      'template': 'template/h5.html'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
